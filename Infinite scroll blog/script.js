adb959a0689757d542404145d1c62938cf6f5980
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2021-04-28 11:22:27 +0300

    Infinit Scroll Blog

---
+++ Infinite scroll blog/script.js
@@ -0,0 +1,75 @@
+const postsContainer = document.getElementById('post-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 3;
+let page = 1;
+
+async function getPosts() {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`,
+	);
+
+	const data = await res.json();
+
+	return data;
+}
+
+//Show posts in DOM
+async function showPosts() {
+	const posts = await getPosts();
+
+	posts.forEach((post) => {
+		const postEl = document.createElement('div');
+		postEl.classList.add('post');
+		postEl.innerHTML = `<div class="number">${post.id}</div>
+        <div class="post-info">
+          <h2 class="post-title">${post.title}</h2>
+          <p class="post-body">${post.body}</p>
+        </div>`;
+		postsContainer.appendChild(postEl);
+	});
+}
+
+showPosts();
+
+const showLoading = () => {
+	loading.classList.add('show');
+	page++;
+	showPosts();
+
+	setTimeout(() => {
+		loading.classList.remove('show');
+	}, 200);
+};
+
+const hideLoading = () => {
+	loading.classList.remove('show');
+};
+
+//The scroll event
+window.addEventListener('scroll', () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	if (scrollTop + clientHeight >= scrollHeight) {
+		showLoading();
+	}
+});
+
+//Filter posts by input
+
+const filterPosts = (e) => {
+	const term = e.target.value.toUpperCase();
+	const posts = document.querySelectorAll('.post');
+
+	posts.forEach((post) => {
+		const title = post.querySelector('.post-title').innerText.toUpperCase();
+		const body = post.querySelector('.post-body').innerText;
+		if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+			post.style.display = 'flex';
+		} else {
+			post.style.display = 'none';
+		}
+	});
+};
+
+filter.addEventListener('input', filterPosts);
