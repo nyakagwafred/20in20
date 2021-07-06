dc9fbcbf46658389a41ee554a3719d38aff83eae
Fred <nyakagwa.fred@gmail.com>
2021-07-06 21:59:48 +0300

    hangman

---
+++ Hangman/script.js
@@ -0,0 +1,46 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const finalMessageRevealWord = document.getElementById(
+	'final-message-reveal-word'
+);
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+//Show hidden word
+const displayWord = () => {
+	wordEl.innerHTML = `
+    ${selectedWord
+			.split('')
+			.map(
+				(letter) => `
+          <span class="letter">
+            ${correctLetters.includes(letter) ? letter : ''}
+          </span>
+        `
+			)
+			.join('')}
+  `;
+
+	const innerWord = wordEl.innerText.replace(/[ \n]/g, '');
+
+	if (innerWord === selectedWord) {
+		finalMessage.innerText = 'Congratulations! You won! 😃';
+		finalMessageRevealWord.innerText = '';
+		popup.style.display = 'flex';
+
+		playable = false;
+	}
+};
+
+displayWord();
