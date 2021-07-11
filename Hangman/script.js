06f8d86371cd3a0bdabab4c89b128dd6d8f89bed
Fred <nyakagwa.fred@gmail.com>
2021-07-11 12:02:46 +0300

    CSS animations

--- Hangman/script.js
+++ Hangman/script.js
@@ -17,8 +17,10 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 const correctLetters = [];
 const wrongLetters = [];
 
+console.log(selectedWord);
+
 //Show hidden word
-const displayWord = () => {
+function displayWord() {
 	wordEl.innerHTML = `
     ${selectedWord
 			.split('')
@@ -38,9 +40,80 @@ const displayWord = () => {
 		finalMessage.innerText = 'Congratulations! You won! 😃';
 		finalMessageRevealWord.innerText = '';
 		popup.style.display = 'flex';
-
-		playable = false;
 	}
-};
+}
+
+//Update the wrong letters
+function updateWrongLettersEL() {
+	//Display wrong letter
+	wrongLettersEl.innerHTML = `
+	${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+	${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+	`;
+
+	//Display hangman parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+
+		if (index < errors) {
+			part.style.display = 'block';
+		} else {
+			part.style.display = 'none';
+		}
+	});
+
+	//Check if lost
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = 'Unfortunately you lost. 😕';
+		popup.style.display = 'flex';
+	}
+}
+
+//Show notification
+function showNotification() {
+	notification.classList.add('show');
+
+	setTimeout(() => {
+		notification.classList.remove('show');
+	}, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', (e) => {
+	if (e.keyCode >= 65 && e.keyCode <= 90) {
+		const letter = e.key;
+
+		if (selectedWord.includes(letter)) {
+			if (!correctLetters.includes(letter)) {
+				correctLetters.push(letter);
+				displayWord();
+			} else {
+				showNotification();
+			}
+		} else {
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter);
+				updateWrongLettersEL();
+			} else {
+				showNotification();
+			}
+		}
+	}
+});
+
+//Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+	//Empty arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+
+	updateWrongLettersEL();
+
+	popup.style.display = 'none';
+});
 
 displayWord();
